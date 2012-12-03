e0196cc2a18f5d92541112d56aa18ffce8cc5c2c
Fernando Briano <fernando@picandocodigo.net>
2012-12-03 18:59:07 -0200

    Algo de vistas: Bootstrap, layout y demás Modelo de usuarios Controladores de Twitter, Issues y Home

---
+++ app/controllers/issues_controller.rb
@@ -0,0 +1,47 @@
+class IssuesController < ApplicationController
+  def new
+  end
+
+  def create
+    issue = Issue.create(params["issue"])
+    render json: issue
+  end
+
+  def show
+    @issue = Issue.find(params["id"])
+    render
+  end
+
+  def index
+    render json: Issue.order(:relevance).limit(20)
+  end
+
+  def edit
+  end
+
+  def update
+    issue = Issue.update_attributes(params["issue"])
+    render json: issue
+  end
+
+  def destroy
+    #Podemos hacer que esto sea solo con rol admin
+    render status: 405
+  end
+
+  def fix
+    # TODO clean variables, control save action
+    issue = Issue.find(params["id"])
+    issue.fixed += 1
+    issue.save
+    redirect_to issue
+  end
+
+  def unfix
+    issue = Issue.find(params["id"])
+    issue.fixed -= 1
+    issue.save
+    redirect_to issue
+  end
+
+end
