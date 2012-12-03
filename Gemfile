36d221e2536d3ed450c58bbbee94151402c69f82
Fernando Briano <fernando@picandocodigo.net>
2012-12-03 13:08:37 -0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.9'
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'therubyracer', :platforms => :ruby
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'debugger'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmbuduyApi::Application.config.secret_token = '3011b70febaa90bcfa1f714510cdff62ae58ce802f37aacc92bcfe90749a83cfebba5b4126c635d380a10ba92cf9e2a7381d680ba5c1171cb25492ed35096b53'
