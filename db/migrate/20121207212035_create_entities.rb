02db24c9bb8bfc22f9b27185bc7fea2bcbc05616
Fernando Briano <fernando@picandocodigo.net>
2012-12-07 19:22:34 -0200

    Agrega entidad entidad

---
+++ db/migrate/20121207212035_create_entities.rb
@@ -0,0 +1,8 @@
+class CreateEntities < ActiveRecord::Migration
+  def change
+    create_table :entities do |t|
+      t.string :name
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121207165208) do
+ActiveRecord::Schema.define(:version => 20121207212035) do
+
+  create_table "entities", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "issues", :force => true do |t|
     t.integer  "relevance",       :default => 0
